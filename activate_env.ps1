f84b14227b972e3a92e49d6be6d6e1a3d70d7cc2
JillesDB <141919440+JillesDB@users.noreply.github.com>
2026-04-23 12:48:44 +1000

    08/04 Pypsa commit

---
+++ activate_env.ps1
@@ -0,0 +1,3 @@
+& "C:\PyEnvs\kupferzell\Scripts\Activate.ps1"
+Set-Location "C:\Users\jille\OneDrive - Danmarks Tekniske Universitet\Laptop Folder\DTU - PhD Work\2026.1 Paper Battery Congestion Alleviation\Code"
+Write-Host "Kupferzell environment active." -ForegroundColor Green
